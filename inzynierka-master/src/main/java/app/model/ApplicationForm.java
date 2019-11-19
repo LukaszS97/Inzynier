522becd5ab6069773c28ba20c7181fe1515676dd
Maciek <maciek-skoczylas@wp.pl>
2019-11-19 13:19:39 +0100

    Add job offer

---
+++ inzynierka-master/src/main/java/app/model/ApplicationForm.java
@@ -0,0 +1,84 @@
+package app.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class ApplicationForm implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String experience;
+    private String phoneNumber;
+    private String education;
+    @ManyToOne
+    @JoinColumn(name = "jobOffer_id")
+    private JobOffer jobOffer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public JobOffer getJobOffer() {
+        return jobOffer;
+    }
+
+    public void setJobOffer(JobOffer jobOffer) {
+        this.jobOffer = jobOffer;
+    }
+
+    public ApplicationForm() {
+    }
+}
